e873589d31ea5521b936fe7ce2b4ccbb63f3d134
Igor Shovkun <igshov@gmail.com>
2022-02-21 14:05:52 -0800

    install script checks directories

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+# build directory
+mkdir -p $HOME/build
 # install zgen minimalist plugin manager for zsh
-git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+if [ ! -d "${HOME}/.zgen" ]
+then
+    git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+fi
+# tmux resurrect
+if [ ! -d "${HOME}/build/tmux-resurrect" ]
+then
+    git clone https://github.com/tmux-plugins/tmux-resurrect
+fi
