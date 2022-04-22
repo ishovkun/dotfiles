81fe42bbe4b4efa6a5d438c16eabe011ae491ac4
Igor Shovkun <igshov@gmail.com>
2022-04-21 22:11:11 -0700

    recover centaur

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -14,14 +14,16 @@
              mac-command-key-is-meta t
              mac-command-modifier 'meta
              mac-option-modifier 'none)
-       (plist-put! +ligatures-extra-symbols
-                   :name          "»"
-                   :src_block     "»"
-                   :src_block_end "«"
-                   :quote         "“"
-                   :quote_end     "”"
-                   :list          "𝕃"
-                   :dot           "•")
+       (set-ligatures! 'python-mode
+         ;; Types
+         :null "None"
+         :true "True" :false "False"
+         :int "int" :str "str"
+         :float "float"
+         :bool "bool"
+         :tuple "tuple"
+         :for "for"
+         :return "return")
        )
   )
 
@@ -64,6 +66,11 @@
  (:map TeX-mode-map
   :nv "j"           #'evil-next-visual-line
   :nv "k"           #'evil-previous-visual-line
+  )
+ (:map prog-mode-map
+  ;;  :i "C-h"         #'backward-delete-char
+  :v "-"         #'evil-last-non-blank
+  :v "="         #'evil-first-non-blank
   )
   :nv "M-H"         #'shrink-window-horizontally
   ;; :nv "S-C-<left>"  #'shrink-window-horizontally
@@ -166,6 +173,21 @@
    :desc "jump to tab 7" :nv "[7" #'awesome-tab-select-visible-tab
    :desc "jump to tab 8" :nv "[8" #'awesome-tab-select-visible-tab
    :desc "jump to tab 9" :nv "[9" #'awesome-tab-select-visible-tab
+   )
+  (:after centaur-tabs
+   :desc "jump to tab 1" :nv "[1" #'centaur-tabs-select-visible-1st-tab
+   :desc "jump to tab 2" :nv "[2" #'centaur-tabs-select-visible-2nd-tab
+   :desc "jump to tab 3" :nv "[3" #'centaur-tabs-select-visible-3rd-tab
+   :desc "jump to tab 4" :nv "[4" #'centaur-tabs-select-visible-tab
+   :desc "jump to tab 5" :nv "[5" #'centaur-tabs-select-visible-tab
+   :desc "jump to tab 6" :nv "[6" #'centaur-tabs-select-visible-tab
+   :desc "jump to tab 7" :nv "[7" #'centaur-tabs-select-visible-tab
+   :desc "jump to tab 8" :nv "[8" #'centaur-tabs-select-visible-tab
+   :desc "jump to tab 9" :nv "[9" #'centaur-tabs-select-visible-tab
+   )
+  (:map vterm-mode-map
+   :desc "C-k" :nvi "C-k"          #'vterm-send-C-k
+   :desc "C-k" :nvi "C-j"          #'vterm-send-C-j
    )
 )
 (map! :leader
@@ -360,111 +382,111 @@
   (define-key company-active-map (kbd "TAB") 'company-yasnippet-or-completion))
 
 ;; tab switching
-(use-package! awesome-tab
-  :config
-  (awesome-tab-mode t)
-  (setq awesome-tab-show-tab-index nil)
-  (setq awesome-tab-height 120)
-  ;; hide tab
-  (defun awesome-tab-hide-tab (x)
-  (let ((name (format "%s" x)))
-    (or
-     (string-prefix-p "*epc" name)
-     (string-prefix-p "*Compile-Log*" name)
-     (string-prefix-p "*lsp" name)
-     (string-prefix-p "*doom*" name)
-     (and (string-prefix-p "magit" name)
-               (not (file-name-extension name)))
-     )))
-  )  ;; end awesome tab
-
-;; (use-package! centaur-tabs
-;;   :init
-;;   (setq centaur-tabs-set-close-button nil)
-;;   (setq centaur-tabs-show-count nil)
+;; (use-package! awesome-tab
 ;;   :config
-;;   ;; (centaur-tabs-init-tabsets-store)
-;;   (centaur-tabs-init-tabsets-store)
-;;   (centaur-tabs-display-update)
-;;   ;; (centaur-tabs-headline-match)
+;;   (awesome-tab-mode t)
+;;   (setq awesome-tab-show-tab-index nil)
+;;   (setq awesome-tab-height 120)
+;;   ;; hide tab
+;;   (defun awesome-tab-hide-tab (x)
+;;   (let ((name (format "%s" x)))
+;;     (or
+;;      (string-prefix-p "*epc" name)
+;;      (string-prefix-p "*Compile-Log*" name)
+;;      (string-prefix-p "*lsp" name)
+;;      (string-prefix-p "*doom*" name)
+;;      (and (string-prefix-p "magit" name)
+;;                (not (file-name-extension name)))
+;;      )))
+;;   )  ;; end awesome tab
 
-;;   (defun wd/get-buffer-persp-group (buffer)
-;;     (let* ((name))
-;;       (dolist (persp (persp-persps))
-;;         (if persp
-;;             (if (persp-contain-buffer-p buffer persp)
-;;                 (setq name (safe-persp-name persp)))))
-;;       name
-;;       ))
+(use-package! centaur-tabs
+  :init
+  (setq centaur-tabs-set-close-button nil)
+  (setq centaur-tabs-show-count nil)
+  :config
+  ;; (centaur-tabs-init-tabsets-store)
+  (centaur-tabs-init-tabsets-store)
+  (centaur-tabs-display-update)
+  ;; (centaur-tabs-headline-match)
 
-;;   (defun centaur-tabs-buffer-groups ()
-;;     (list
-;;      (cond
-;;       ((or (string-equal "*" (substring (buffer-name) 0 1))
-;;          (memq major-mode '(magit-process-mode
-;;                             magit-status-mode
-;;                             magit-diff-mode
-;;                             magit-log-mode
-;;                             magit-file-mode
-;;                             magit-blob-mode
-;;                             magit-blame-mode
-;;                             )))
-;;      "Emacs")
-;;     ((derived-mode-p 'eshell-mode)
-;;      "EShell")
-;;     ;; ((derived-mode-p 'emacs-lisp-mode) ;; do not separate elisp filed from everything else
-;;     ;;  "Elisp")
-;;     ((derived-mode-p 'dired-mode)
-;;      "Dired")
-;;     ((memq major-mode '(org-mode org-agenda-mode diary-mode))
-;;      "OrgMode")
-;;     (t
-;;      (let ((name (wd/get-buffer-persp-group (current-buffer))))
-;;        (if name
-;;            name
-;;          ;;(centaur-tabs-get-group-name (current-buffer))
-;;          "Other"
-;;          )
-;;        ))))
-;;   )
+  (defun wd/get-buffer-persp-group (buffer)
+    (let* ((name))
+      (dolist (persp (persp-persps))
+        (if persp
+            (if (persp-contain-buffer-p buffer persp)
+                (setq name (safe-persp-name persp)))))
+      name
+      ))
 
-;;   (defun centaur-tabs-hide-tab (x)
-;;     "Do no to show buffer X in tabs."
-;;     (let ((name (format "%s" x)))
-;;       (or
-;;        ;; Current window is not dedicated window.
-;;        (window-dedicated-p (selected-window))
+  (defun centaur-tabs-buffer-groups ()
+    (list
+     (cond
+      ((or (string-equal "*" (substring (buffer-name) 0 1))
+         (memq major-mode '(magit-process-mode
+                            magit-status-mode
+                            magit-diff-mode
+                            magit-log-mode
+                            magit-file-mode
+                            magit-blob-mode
+                            magit-blame-mode
+                            )))
+     "Emacs")
+    ((derived-mode-p 'eshell-mode)
+     "EShell")
+    ;; ((derived-mode-p 'emacs-lisp-mode) ;; do not separate elisp filed from everything else
+    ;;  "Elisp")
+    ((derived-mode-p 'dired-mode)
+     "Dired")
+    ((memq major-mode '(org-mode org-agenda-mode diary-mode))
+     "OrgMode")
+    (t
+     (let ((name (wd/get-buffer-persp-group (current-buffer))))
+       (if name
+           name
+         ;;(centaur-tabs-get-group-name (current-buffer))
+         "Other"
+         )
+       ))))
+  )
 
-;;        ;; Buffer name not match below blacklist.
-;;        (string-prefix-p "*epc" name)
-;;        (string-prefix-p "*helm" name)
-;;        (string-prefix-p "*Helm" name)
-;;        (string-prefix-p "*Compile-Log*" name)
-;;        (string-prefix-p "*lsp" name)
-;;        (string-prefix-p "*company" name)
-;;        ;; (string-prefix-p "*vterm" name)
-;;        (string-prefix-p "*ccls" name)
-;;        (string-prefix-p "*Flycheck" name)
-;;        (string-prefix-p "*quickrun" name)
-;;        (string-prefix-p "*Calculator*" name)
-;;        (string-prefix-p "*Calc Trail*" name)
-;;        (string-prefix-p "*anaconda-mode" name)
-;;        (string-prefix-p "*compilation" name)
-;;        (string-prefix-p "*Deft*" name)
-;;        (string-prefix-p "*tramp" name)
-;;        (string-prefix-p " *Mini" name)
-;;        (string-prefix-p "*help" name)
-;;        (string-prefix-p "*straight" name)
-;;        (string-prefix-p " *temp" name)
-;;        (string-prefix-p "*Help" name)
-;;        ;; (string-prefix-p "*mybuf" name)
+  (defun centaur-tabs-hide-tab (x)
+    "Do no to show buffer X in tabs."
+    (let ((name (format "%s" x)))
+      (or
+       ;; Current window is not dedicated window.
+       (window-dedicated-p (selected-window))
 
-;;        ;; Is not magit buffer.
-;;        (and (string-prefix-p "magit" name)
-;;             (not (file-name-extension name)))
-;;        )))
-;;   (centaur-tabs-mode t)
-;; )
+       ;; Buffer name not match below blacklist.
+       (string-prefix-p "*epc" name)
+       (string-prefix-p "*helm" name)
+       (string-prefix-p "*Helm" name)
+       (string-prefix-p "*Compile-Log*" name)
+       (string-prefix-p "*lsp" name)
+       (string-prefix-p "*company" name)
+       ;; (string-prefix-p "*vterm" name)
+       (string-prefix-p "*ccls" name)
+       (string-prefix-p "*Flycheck" name)
+       (string-prefix-p "*quickrun" name)
+       (string-prefix-p "*Calculator*" name)
+       (string-prefix-p "*Calc Trail*" name)
+       (string-prefix-p "*anaconda-mode" name)
+       (string-prefix-p "*compilation" name)
+       (string-prefix-p "*Deft*" name)
+       (string-prefix-p "*tramp" name)
+       (string-prefix-p " *Mini" name)
+       (string-prefix-p "*help" name)
+       (string-prefix-p "*straight" name)
+       (string-prefix-p " *temp" name)
+       (string-prefix-p "*Help" name)
+       ;; (string-prefix-p "*mybuf" name)
+
+       ;; Is not magit buffer.
+       (and (string-prefix-p "magit" name)
+            (not (file-name-extension name)))
+       )))
+  (centaur-tabs-mode t)
+)
 ;; ------------------------------ GUI -----------------------------------------
 ;; tweaks
 (setq display-line-numbers-type 'relative)
@@ -601,7 +623,7 @@
 (use-package! yascroll
   :init
   (setq yascroll:delay-to-hide nil
-        yascroll:scroll-bar 'text-area)
+        yascroll:scroll-bar 'right-fringe) ; right-fringe / text-area
   :config
   (add-hook 'prog-mode-hook 'yascroll-bar-mode)
   (add-hook 'text-mode-hook 'yascroll-bar-mode)
@@ -896,37 +918,46 @@
   (setq ein:output-area-inlined-images t)
   )
 ;; ----------------------------------- Shell ---------------------------------
-(when (display-graphic-p)
-  (use-package mini-modeline
-    :after doom-modeline
-    :config
-    (doom-modeline-def-modeline 'minibuffer-line
-      '(matches remote-host persp-name major-mode indent-info vcs lsp checker fancy-modals)
-      )
-    (setq mini-modeline-l-format '("%e" mode-line-front-space
-                                   ;; (:eval (doom-modeline--anzu))
-                                   ;; (:eval (doom-modeline--buffer-mode-icon))
-                                   (:eval (doom-modeline--macro-recording))
-                                   (:eval (doom-modeline--buffer-name))
-                                   ))
-    (setq mini-modeline-r-format '(:eval (doom-modeline-format--minibuffer-line)))
-    (setq mini-modeline-right-padding 0 mini-modeline-enhance-visual t)
-    ;; shut up minibuffer messages
-    ;; (setq inhibit-message nil)
-    (global-eldoc-mode -1) ;; disable useful eldoc messages
+;; (when (display-graphic-p)
+;;   (use-package mini-modeline
+;;     :after doom-modeline
+;;     :config
+;;     (doom-modeline-def-modeline 'minibuffer-line
+;;       '(matches remote-host persp-name major-mode indent-info vcs lsp checker fancy-modals)
+;;       )
+;;     (setq mini-modeline-l-format '("%e" mode-line-front-space
+;;                                    ;; (:eval (doom-modeline--anzu))
+;;                                    ;; (:eval (doom-modeline--buffer-mode-icon))
+;;                                    (:eval (doom-modeline--macro-recording))
+;;                                    (:eval (doom-modeline--buffer-name))
+;;                                    ))
+;;     (setq mini-modeline-r-format '(:eval (doom-modeline-format--minibuffer-line)))
+;;     (setq mini-modeline-right-padding 0 mini-modeline-enhance-visual t)
 
-    (global-hide-mode-line-mode)
-    (set-face-attribute 'mode-line nil :box nil :height 1)
-    (set-face-attribute 'minibuffer-prompt nil :background (face-background 'tab-bar))
-    (setq mini-modeline-display-gui-line nil)
-    (remove-hook 'dired-mode-hook #'doom-modeline-set-project-modeline t)
-    (mini-modeline-mode t)
-    )
-)
+;;     ;; (setq mini-modeline--cache
+;;     ;;   (mini-modeline--multi-lr-render
+;;     ;;    (format-mode-line mini-modeline-l-format)
+;;     ;;    (if mini-modeline--msg
+;;     ;;        (format-mode-line '(:eval (mini-modeline-msg)))
+;;     ;;      (format-mode-line mini-modeline-r-format))))
+
+;;     ;; shut up minibuffer messages
+;;     ;; (setq inhibit-message nil)
+;;     (global-eldoc-mode -1) ;; disable useful eldoc messages
+
+;;     (global-hide-mode-line-mode)
+;;     (set-face-attribute 'mode-line nil :box nil :height 1)
+;;     (set-face-attribute 'minibuffer-prompt nil :background (face-background 'tab-bar))
+;;     (setq mini-modeline-display-gui-line nil)
+;;     (remove-hook 'dired-mode-hook #'doom-modeline-set-project-modeline t)
+;;     (mini-modeline-mode t)
+;;     )
+;; )
 ;; --------------------------------- Fixes -----------------------------------
 ;; (setq evil-respect-visual-line-mode t)
 ;; (setq evil-respect-visual-line-mode 0) ;; otherwise  deletes line on cc
 (setq evil-move-cursor-back nil)
+(setq org-startup-truncated nil)
 ;; (setq-default truncate-lines nil)
 ;; make compilation buffer stick to the frame
 ;; (push '("\\*compilation\\*" . (nil (reusable-frames . t))) display-buffer-alist)
