efd86abb114fb33c4984e99e52bba32656290c5c
Igor Shovkun <igshov@gmail.com>
2022-08-06 07:00:56 -0700

    doom: no rust + python conda modules

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -167,12 +167,12 @@
        ;;php                   ; perl's insecure younger brother
        ;;plantuml              ; diagrams for confusing people more
        ;;purescript            ; javascript, but functional
-       (python + lsp)          ; beautiful is better than ugly
+       (python +lsp +conda)    ; beautiful is better than ugly
        ;;qt                    ; the 'cutest' gui framework ever
        ;;racket                ; a DSL for DSLs
        ;;rest                  ; Emacs as a REST client
        ;;ruby                  ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)             ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;; (rust +lsp)             ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala                 ; java, but good
        sh                      ; she sells {ba,z,fi}sh shells on the C xor
        ;;solidity              ; do you need a blockchain? No.
