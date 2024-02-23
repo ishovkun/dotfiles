232e1b2085e31160b5d5758c0938886afd82331b
Igor Shovkun <igshov@gmail.com>
2024-02-23 12:13:06 -0800

    doom tty is part of :os

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -103,7 +103,6 @@
         ;;+forge
               )                ; a git porcelain for Emacs
        make                    ; run make tasks from Emacs
-       tty                     ; improve the terminal Emacs experience
        ;;pass                  ; password manager for nerds
        pdf                     ; pdf enhancements
        ;;prodigy               ; FIXME managing external services & code builders
