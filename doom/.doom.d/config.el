3074c26cf48220a561da8322abeb7375a84f54f2
Igor Shovkun <igshov@gmail.com>
2024-03-21 10:59:02 -0700

    add cuda mode hook linum

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -556,7 +556,7 @@
 ;; tweaks
 (setq display-line-numbers-type 'relative)
 ;; (add-hook 'c-mode-common-hook 'display-line-numbers-mode)
-;; (add-hook 'cuda-mode-hook 'display-line-numbers-mode)
+(add-hook 'cuda-mode-hook 'display-line-numbers-mode)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 ;; (setq confirm-kill-emacs)
